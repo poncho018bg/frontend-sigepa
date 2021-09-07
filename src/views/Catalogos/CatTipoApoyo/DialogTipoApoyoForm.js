d88d78f0564d235834560c1968e68795e69d8fc7
poncho018bg <alfons018pbg@gmail.com>
2021-09-06 20:34:00 -0500

    catalogo de tipo apoyo

--- src/views/Catalogos/CatTipoApoyo/DialogTipoApoyoForm.js
+++ src/views/Catalogos/CatTipoApoyo/DialogTipoApoyoForm.js
@@ -13,6 +13,9 @@ import { TiposApoyosContext } from 'contexts/catalogos/tiposApoyosContext';
 import { EdadesBeneficiariosContext } from 'contexts/catalogos/edadesBeneficiariosContext';
 import { TiposBeneficiariosContext } from 'contexts/catalogos/tiposBeneficiariosContext';
 import { PeriodicidadApoyosContext } from 'contexts/catalogos/periodicidadApoyosContext';
+import { DocumentosContext } from 'contexts/catalogos/documentosContext';
+import moment from 'moment';
+import 'moment/locale/es';
 const useStyles = makeStyles(stylesArchivo);
 
 
@@ -22,25 +25,27 @@ const initTipoApoyo = {
     nombrePrograma: '',
     clavePrograma: '',
     descTipoApoyo: '',
-    estatusActivo: boolean,
-    estatusInactivo: boolean,
+    estatus: '',
+    visita: '',
     idTipoApoyo: '',
-    vigenciaDesde: '',
-    vigenciaHasta: '',
+    vigenciaDesde: moment(new Date()).format("yyyy-MM-dd") ,
+    vigenciaHasta: moment(new Date()).format("yyyy-MM-dd"),
     idRangoEdad: '',
     idTipoBeneficiario: '',
     cantidadPesos: '',
     desApoyoEspecia: '',
-    idPeriodicidad: ''
+    idPeriodicidad: '',
+    observaciones: null,
+    formaEntrega: ''
 }
 
 function not(a, b) {
     return a.filter((value) => b.indexOf(value) === -1);
-  }
-  
-  function intersection(a, b) {
+}
+
+function intersection(a, b) {
     return a.filter((value) => b.indexOf(value) !== -1);
-  }
+}
 
 export const DialogTipoApoyoForm = (props) => {
 
@@ -52,8 +57,8 @@ export const DialogTipoApoyoForm = (props) => {
         nombrePrograma,
         clavePrograma,
         descTipoApoyo,
-        estatusActivo,
-        estatusInactivo,
+        estatus,
+        visita,
         idTipoApoyo,
         vigenciaDesde,
         vigenciaHasta,
@@ -61,20 +66,29 @@ export const DialogTipoApoyoForm = (props) => {
         idTipoBeneficiario,
         cantidadPesos,
         desApoyoEspecia,
-        idPeriodicidad
+        idPeriodicidad,
+        observaciones,
+        formaEntrega
     } = formValues;
     const [errors, setErrors] = useState({});
+    const { tipoApoyoEditar } = useSelector(state => state.tipoApoyo);
     const { getTiposApoyos, tiposApoyosList } = useContext(TiposApoyosContext);
     const { getEdadesBeneficiarios, edadesBeneficiariosList } = useContext(EdadesBeneficiariosContext);
     const { getTipoBeneficiarios, tiposBeneficiariosList } = useContext(TiposBeneficiariosContext);
     const { getPeriodicidadApoyos, periodicidadApoyosList } = useContext(PeriodicidadApoyosContext);
+    const { getDocumentos, documentosList } = useContext(DocumentosContext);
 
     const [checked, setChecked] = React.useState([]);
-    const [left, setLeft] = React.useState([0, 1, 2, 3]);
-    const [right, setRight] = React.useState([4, 5, 6, 7]);
+    const [left, setLeft] = React.useState([]);
+    const [right, setRight] = React.useState([]);
 
     const leftChecked = intersection(checked, left);
     const rightChecked = intersection(checked, right);
+    const [estatusf, setEstatusf] = React.useState('Activo');
+    const [visitaf, setVisitaf] = React.useState('SI');
+    const [state, setState] = React.useState({
+        checkedA: false
+    });
 
 
     useEffect(() => {
@@ -82,8 +96,13 @@ export const DialogTipoApoyoForm = (props) => {
         getEdadesBeneficiarios();
         getTipoBeneficiarios();
         getPeriodicidadApoyos();
-
+        getDocumentos();
+        setLeft(documentosList)
     }, []);
+
+    useEffect(() => {
+        setLeft(documentosList)
+    }, [documentosList]);
 
 
     const handleClose = () => {
@@ -105,24 +124,24 @@ export const DialogTipoApoyoForm = (props) => {
         props.setShowDialogForm(false);
 
     }
+    const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
 
     const handleToggle = (value) => () => {
         const currentIndex = checked.indexOf(value);
         const newChecked = [...checked];
-    
-        if (currentIndex === -1) {
-          newChecked.push(value);
-        } else {
-          newChecked.splice(currentIndex, 1);
-        }
-    
-        setChecked(newChecked);
-      };
 
-    const handleAllRight = () => {
-        setRight(right.concat(left));
-        setLeft([]);
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
     };
+
+
 
     const handleCheckedRight = () => {
         setRight(right.concat(leftChecked));
@@ -136,10 +155,6 @@ export const DialogTipoApoyoForm = (props) => {
         setChecked(not(checked, rightChecked));
     };
 
-    const handleAllLeft = () => {
-        setLeft(left.concat(right));
-        setRight([]);
-    };
 
     const customList = (items) => (
         <Paper className={classes.paper}>
@@ -157,7 +172,7 @@ export const DialogTipoApoyoForm = (props) => {
                                     inputProps={{ 'aria-labelledby': labelId }}
                                 />
                             </ListItemIcon>
-                            <ListItemText id={labelId} primary={`List item ${value + 1}`} />
+                            <ListItemText id={labelId} primary={`${value.dsdocumento}`} />
                         </ListItem>
                     );
                 })}
@@ -177,7 +192,7 @@ export const DialogTipoApoyoForm = (props) => {
             aria-labelledby="customized-dialog-title"
             open={props.showDialogForm}
         > <form onSubmit={handleAgregar}>
-                <DialogTitle id="form-dialog-title">{(true) ? 'Editar Tipo de Apoyo' : 'Alta Tipo de Apoyo'} </DialogTitle>
+                <DialogTitle id="form-dialog-title">{(tipoApoyoEditar) ? 'Editar Tipo de Apoyo' : 'Alta Tipo de Apoyo'} </DialogTitle>
 
                 <DialogContent>
 
@@ -190,6 +205,7 @@ export const DialogTipoApoyoForm = (props) => {
                         value={nombreTipoApoyo}
                         onChange={handleInputChange}
                         fullWidth
+                        inputProps={{ maxLength: 500 }}
                     />
                     {errors.nombreTipoApoyo && <FormHelperText error={errors.nombreTipoApoyo !== null && errors.nombreTipoApoyo !== undefined}>{errors.nombreTipoApoyo}</FormHelperText>}
 
@@ -206,6 +222,7 @@ export const DialogTipoApoyoForm = (props) => {
                         value={nombrePrograma}
                         onChange={handleInputChange}
                         fullWidth
+                        inputProps={{ maxLength: 500 }}
                     />
                     {errors.nombrePrograma && <FormHelperText error={errors.nombrePrograma !== null && errors.nombrePrograma !== undefined}>{errors.nombrePrograma}</FormHelperText>}
 
@@ -222,6 +239,7 @@ export const DialogTipoApoyoForm = (props) => {
                         value={clavePrograma}
                         onChange={handleInputChange}
                         fullWidth
+                        inputProps={{ maxLength: 100 }}
                     />
                     {errors.clavePrograma && <FormHelperText error={errors.clavePrograma !== null && errors.clavePrograma !== undefined}>{errors.clavePrograma}</FormHelperText>}
 
@@ -238,6 +256,7 @@ export const DialogTipoApoyoForm = (props) => {
                         value={descTipoApoyo}
                         onChange={handleInputChange}
                         fullWidth
+                        inputProps={{ maxLength: 300 }}
                     />
                     {errors.descTipoApoyo && <FormHelperText error={errors.descTipoApoyo !== null && errors.descTipoApoyo !== undefined}>{errors.descTipoApoyo}</FormHelperText>}
 
@@ -245,9 +264,9 @@ export const DialogTipoApoyoForm = (props) => {
 
                 <DialogContent>
                     <FormLabel component="legend">Estatus </FormLabel>
-                    <RadioGroup row aria-label="position" name="position" defaultValue="top">
-                        <FormControlLabel value={estatusActivo} control={<Radio color="primary" />} label="Activo" />
-                        <FormControlLabel value={estatusInactivo} control={<Radio color="primary" />} label="Inactivo" />
+                    <RadioGroup row aria-label="position" name="estatus" defaultValue="top" value={estatus} onChange={handleInputChange} >
+                        <FormControlLabel value="true" control={<Radio color="primary" />} label="Activo" />
+                        <FormControlLabel value="false" control={<Radio color="primary" />} label="Inactivo" />
                     </RadioGroup>
                 </DialogContent>
 
@@ -282,30 +301,41 @@ export const DialogTipoApoyoForm = (props) => {
                 </DialogContent>
 
                 <DialogContent>
-                    <TextField
-                        id="vigenciaDesde"
-                        label="Vigencia del tipo apoyo desde"
-                        type="date"
-                        defaultValue="2017-05-24"
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        value={vigenciaDesde}
-                    />
-                </DialogContent>
-                <DialogContent>
-                    <TextField
-                        id="vigenciaHasta"
-                        label="Vigencia del tipo apoyo hasta"
-                        type="date"
-                        defaultValue="2017-05-24"
-                        className={classes.textField}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        value={vigenciaHasta}
-                    />
+                    <div><FormLabel component="legend">Vigencia del tipo apoyo </FormLabel></div>
+                    <div>
+                        <TextField
+                            id="vigenciaDesde"
+                            label="Desde"
+                            type="date"
+                           
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            value={vigenciaDesde}
+                            name="vigenciaDesde"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            id="vigenciaHasta"
+                            label="Hasta"
+                            type="date"
+                            
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            value={vigenciaHasta}
+                            name="vigenciaHasta"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
+
+
+
                 </DialogContent>
 
 
@@ -395,6 +425,7 @@ export const DialogTipoApoyoForm = (props) => {
                         value={desApoyoEspecia}
                         onChange={handleInputChange}
                         fullWidth
+                        inputProps={{ maxLength: 100 }}
                     />
                     {errors.desApoyoEspecia && <FormHelperText error={errors.desApoyoEspecia !== null && errors.desApoyoEspecia !== undefined}>{errors.desApoyoEspecia}</FormHelperText>}
 
@@ -402,9 +433,9 @@ export const DialogTipoApoyoForm = (props) => {
 
                 <DialogContent>
                     <FormLabel component="legend">Requiere Visita Obligatoria</FormLabel>
-                    <RadioGroup row aria-label="position" name="position" defaultValue="top">
-                        <FormControlLabel value={estatusActivo} control={<Radio color="primary" />} label="Activo" />
-                        <FormControlLabel value={estatusInactivo} control={<Radio color="primary" />} label="Inactivo" />
+                    <RadioGroup row aria-label="position" name="visita" defaultValue="top" value={visita} onChange={handleInputChange} >
+                        <FormControlLabel value="true" control={<Radio color="primary" />} label="Activo" />
+                        <FormControlLabel value="false" control={<Radio color="primary" />} label="Inactivo" />
                     </RadioGroup>
                 </DialogContent>
 
@@ -417,20 +448,12 @@ export const DialogTipoApoyoForm = (props) => {
                         justifyContent="center"
                         alignItems="center"
                         className={classes.root}
+                        fullWidth
                     >
                         <Grid item>{customList(left)}</Grid>
                         <Grid item>
                             <Grid container direction="column" alignItems="center">
-                                <Button
-                                    variant="outlined"
-                                    size="small"
-                                    className={classes.button}
-                                    onClick={handleAllRight}
-                                    disabled={left.length === 0}
-                                    aria-label="move all right"
-                                >
-                                    ≫
-                                </Button>
+
                                 <Button
                                     variant="outlined"
                                     size="small"
@@ -451,16 +474,7 @@ export const DialogTipoApoyoForm = (props) => {
                                 >
                                     &lt;
                                 </Button>
-                                <Button
-                                    variant="outlined"
-                                    size="small"
-                                    className={classes.button}
-                                    onClick={handleAllLeft}
-                                    disabled={right.length === 0}
-                                    aria-label="move all left"
-                                >
-                                    ≪
-                                </Button>
+
                             </Grid>
                         </Grid>
                         <Grid item>{customList(right)}</Grid>
@@ -499,9 +513,10 @@ export const DialogTipoApoyoForm = (props) => {
 
                 <DialogContent>
 
-                    <RadioGroup row aria-label="position" name="position" defaultValue="top">
-                        <FormControlLabel value={estatusActivo} control={<Radio color="primary" />} label="Forma de entrega de apoyo por exhibición" />
-                    </RadioGroup>
+                    <FormControlLabel
+                        control={<Checkbox checked={state.checkedA} onChange={handleChange} name="checkedA" />}
+                        label="Forma de entrega de apoyo por exhibición"
+                    />
                 </DialogContent>
 
                 <DialogContent>
@@ -533,6 +548,24 @@ export const DialogTipoApoyoForm = (props) => {
 
                     </TextField>
                     {errors.idPeriodicidad && <FormHelperText error={errors.idPeriodicidad}>{errors.idPeriodicidad}</FormHelperText>}
+
+                </DialogContent>
+
+                <DialogContent>
+
+
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Observaciones"
+                        multiline
+                        rows={4}
+
+                        variant="outlined"
+                        name="observaciones"
+                        value={observaciones}
+                        fullWidth
+                        inputProps={{ maxLength: 500 }}
+                    />
 
                 </DialogContent>
 
