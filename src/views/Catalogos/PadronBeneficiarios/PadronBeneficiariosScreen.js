bf68ea207e93d2c613b81087aaca11c332fb7663
davleax <aletd87@gmail.com>
2021-09-07 12:59:20 -0500

    padron de beneficiarios grid

--- src/views/Catalogos/PadronBeneficiarios/PadronBeneficiariosScreen.js
+++ src/views/Catalogos/PadronBeneficiarios/PadronBeneficiariosScreen.js
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
+import GridItem from "components/Grid/GridItem.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+import CardActions from '@material-ui/core/CardActions';
+import { Grid } from '@material-ui/core';
+import { makeStyles, Table, TableBody, TableCell, TableHead, TablePagination, TableRow } from '@material-ui/core';
+import { stylesArchivo } from 'css/stylesArchivo';
+import SearchBar from 'material-ui-search-bar';
+const useStyles = makeStyles(stylesArchivo);
 
 export const PadronBeneficiariosScreen = () => {
+    const classes = useStyles();
+    const [searched, setSearched] = useState('');
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [showDialogForm, setShowDialogForm] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [paginas, setPaginas] = useState([]);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+  
+
     return (
-        <div>
-            PadronBeneficiariosScreen
-        </div>
+        <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Padrón de beneficiarios</h4>
+            <p className={classes.cardCategoryWhite}>
+              Pantalla que permite ver el padron de beneficiarios
+            </p>
+            <CardActions>
+            <Grid container spacing={3}>
+          
+            <Grid item xs={6}>
+              <SearchBar
+                placeholder="Buscar"
+                value={searched}
+                onChange={(searchVal) => setSearched(searchVal)}
+                onCancelSearch={() => setSearched('')}
+              />
+            </Grid>
+            </Grid>
+            </CardActions>
+          </CardHeader>
+          <CardBody>
+              < Table stickyHeader aria-label="sticky table" >
+                < TableHead >
+                  < TableRow key="898as" >
+                    < TableCell > Beneficiario</TableCell >
+                    < TableCell > CURP  </TableCell >
+                    < TableCell> Apoyo Otorgado</TableCell >
+                    <TableCell>Tipo de Apoyo</TableCell>
+                    < TableCell> Año Registro Programa</TableCell >
+                    < TableCell>Periocidad</TableCell >
+                    < TableCell>Nùmero de veces</TableCell >
+                  </TableRow >
+                </TableHead >
+                < TableBody >
+                  {
+                       (searched ?
+                        paginas.filter(row => row.dsPagina ?
+                          row.dsPagina.toLowerCase().includes(searched.toLowerCase()) : null)
+                        : paginas
+                      ).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                        return (
+                          <Pagina 
+                             key={row.id}
+                            pagina={row}
+                          />
+                        );
+                      })
+                    }
+              </TableBody >
+            </ Table>
+            < TablePagination
+                rowsPerPageOptions={[5, 10, 15]}
+                component="div"
+                labelRowsPerPage="Registros por página"
+                count={paginas.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+              />
+          </CardBody>
+        </Card>
+      </GridItem>
     )
 }
