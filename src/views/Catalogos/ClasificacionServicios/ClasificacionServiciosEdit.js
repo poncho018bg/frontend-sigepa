deacd4ed4fc4a5ac85c281e8e5c1b6c97ca16408
poncho018bg <alfons018pbg@gmail.com>
2021-10-19 15:51:19 -0500

    corrección de incidencias

--- src/views/Catalogos/ClasificacionServicios/ClasificacionServiciosEdit.js
+++ src/views/Catalogos/ClasificacionServicios/ClasificacionServiciosEdit.js
@@ -100,6 +100,7 @@ export const ClasificacionServiciosEdit = ({ clasificacionServiciosSeleccionado 
                                 onChange={props.handleChange}
                                 onBlur={props.handleBlur}
                                 value={props.values.dsclasificacionservicio}
+                                inputProps={{ maxLength: 100 }}
                             />
                             {props.touched.dsclasificacionservicio && props.errors.dsclasificacionservicio ? (
                                 <FormHelperText error={props.errors.dsclasificacionservicio}>{props.errors.dsclasificacionservicio}</FormHelperText>
@@ -116,6 +117,7 @@ export const ClasificacionServiciosEdit = ({ clasificacionServiciosSeleccionado 
                                 onChange={props.handleChange}
                                 onBlur={props.handleBlur}
                                 value={props.values.dsabreviatura}
+                                inputProps={{ maxLength: 20 }}
                             />
                             {props.touched.dsabreviatura && props.errors.dsabreviatura ? (
                                 <FormHelperText error={props.errors.dsabreviatura}>{props.errors.dsabreviatura}</FormHelperText>
--- src/views/Catalogos/ClasificacionServicios/ClasificacionServiciosForm.js
+++ src/views/Catalogos/ClasificacionServicios/ClasificacionServiciosForm.js
@@ -73,9 +73,11 @@ export const ClasificacionServiciosForm = () => {
         },
         validationSchema: Yup.object({
             dsclasificacionservicio: Yup.string()
-                .required('La clasificacion es obligatoria'),
+                .required('La clasificacion es obligatoria')
+                .matches(/^[a-zA-Z_.-\sñÑ]*$/, `${t('msg.nocarateresespeciales')}`),
             dsabreviatura: Yup.string()
-                .required('La abreviatura es obligatoria'),
+                .required('La abreviatura es obligatoria')
+                .matches(/^[a-zA-Z_.-\sñÑ]*$/, `${t('msg.nocarateresespeciales')}`),
 
         }),
         onSubmit: async valores => {
@@ -98,6 +100,7 @@ export const ClasificacionServiciosForm = () => {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.dsclasificacionservicio}
+                    inputProps={{ maxLength: 100 }}
                 />
                 {formik.touched.dsclasificacionservicio && formik.errors.dsclasificacionservicio ? (
                     <FormHelperText error={formik.errors.dsclasificacionservicio}>{formik.errors.dsclasificacionservicio}</FormHelperText>
@@ -114,6 +117,7 @@ export const ClasificacionServiciosForm = () => {
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
                     value={formik.values.dsabreviatura}
+                    inputProps={{ maxLength: 20 }}
                 />
                 {formik.touched.dsabreviatura && formik.errors.dsabreviatura ? (
                     <FormHelperText error={formik.errors.dsabreviatura}>{formik.errors.dsabreviatura}</FormHelperText>
