238b1bdabddf85af76d4e6eaf366f421b01b8261
Sergio Carreon <otilbaid@gmail.com>
2021-12-01 17:03:40 -0600

    corrección en la pantalla de validacion

--- src/views/RegistroSolicitudContacto/ValidarProgramaMonetario.js
+++ src/views/RegistroSolicitudContacto/ValidarProgramaMonetario.js
@@ -7,31 +7,9 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from "@material-ui/core/styles";
-import customSelectStyle from "assets/jss/material-dashboard-pro-react/customSelectStyle.js";
-import customCheckboxRadioSwitch from "assets/jss/material-dashboard-pro-react/customCheckboxRadioSwitch.js";
-const useStyles = makeStyles(styles);
-const styles = {
-    infoText: {
-        fontWeight: "300",
-        margin: "10px 0 30px",
-        textAlign: "center",
-    },
-    inputAdornmentIcon: {
-        color: "#555",
-    },
-    choiche: {
-        textAlign: "center",
-        cursor: "pointer",
-        marginTop: "20px",
-    },
-    ...customSelectStyle,
-    ...customCheckboxRadioSwitch,
-};
 
 
 const ValidarProgramaMonetario = ({ curp, edadValida, children, setActivar }) => {
-    const classes = useStyles();
     const { beneficiarioMonetario, getBeneficiarioMonetario } = useContext(RegistroSolicitudContext)
     if (edadValida !== undefined) {
         console.log("Monetario curp", curp)
@@ -48,7 +26,7 @@ const ValidarProgramaMonetario = ({ curp, edadValida, children, setActivar }) =>
                 <GridItem xs={12} sm={12} md={12}>
                     <Card>
                         <CardHeader color="primary">
-                            <h4 className={classes.cardTitleWhite}>Registro de Programa de Apoyo</h4>
+                            <h4>Registro de Programa de Apoyo</h4>
                         </CardHeader>
                         <CardBody>
                             <Typography variant="body1" color="text.secondary">
