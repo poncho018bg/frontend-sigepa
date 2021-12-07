a5e4d0532378cd5619f47834a2493548b37fcd8b
Sergio Carreon <otilbaid@gmail.com>
2021-12-07 10:17:13 -0600

    correccion de errores registro solicitud admin

--- src/views/RegistroSolicitudContacto/RegistroFinalizado.js
+++ src/views/RegistroSolicitudContacto/RegistroFinalizado.js
@@ -80,12 +80,21 @@ export const RegistroFinalizado = (props) => {
     useEffect(() => {
         console.log('Finalizar')
         console.log('origen=>',origen)
-        let folios = {
-            idPrograma: idPrograma,
-            idBeneficiario: beneficiario.id,
-            idUsuario: sessionStorage.getItem('idUSuario'),
-            idOrigen:origen?.id
-
+        let folios;
+        if (beneficiario.id !== undefined) {
+            folios = {
+                idPrograma: idPrograma,
+                idBeneficiario: beneficiario?.id,
+                idUsuario: '1fd03e93-887c-4c48-9a25-71097e7f71f4',
+                idOrigen: origen?.id
+            }
+        } else {
+            folios = {
+                idPrograma: idPrograma,
+                idBeneficiario: beneficiario[0]?.id,
+                idUsuario: '1fd03e93-887c-4c48-9a25-71097e7f71f4',
+                idOrigen: origen?.id
+            }
         }
         registrarSolicitudFolio(folios).then(response => {
 
