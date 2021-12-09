3ffe1a8259f8ec0bcc9cb1ebd0535d04a88fc79b
poncho018bg <alfons018pbg@gmail.com>
2021-12-08 21:40:01 -0600

    Datos dinamicos

--- src/views/Expediente/DetalleExpDig.js
+++ src/views/Expediente/DetalleExpDig.js
@@ -290,6 +290,8 @@ export const DetalleExpDig = (props) => {
                     setShowDialogForm={setShowDialogForm}
                     etapaSeleccionada={props.etapaSeleccionada}
                     idExpediente={idExpediente}
+                    idBeneficiario={idBeneficiario}
+                    setIdentPrograma={setIdentPrograma}
                    
                 />
             </Grid>
