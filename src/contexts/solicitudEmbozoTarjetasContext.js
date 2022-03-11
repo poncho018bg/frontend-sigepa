f07b1bc4b9c014da1cf643ff9c356616d1070d91
Sergio Carreon <otilbaid@gmail.com>
2022-03-10 19:40:16 -0600

    primera version selected all y select uno por uno

--- src/contexts/solicitudEmbozoTarjetasContext.js
+++ src/contexts/solicitudEmbozoTarjetasContext.js
@@ -12,8 +12,9 @@ import {
   AGREGAR_SOLICITUD_FOLIO_ERROR,
   GUARDAR_EMBOZO_TARJETAS,
   GET_ETIQUETADO_TARJETAS,
-  GUARDAR_ETIQUETADO_TARJETAS
+  GUARDAR_ETIQUETADO_TARJETAS,
 } from "types/actionTypes";
+import { InsertDriveFileTwoTone } from "@material-ui/icons";
 
 const baseUrl = process.env.REACT_APP_API_URL;
 const baseUrlPublico = process.env.REACT_APP_API_PUBLICO_URL;
@@ -153,11 +154,12 @@ export const SolicitudEmbozoTarjetasContextProvider = (props) => {
     }
   };
 
+  /**
+   * Busca todos los beneficiarios listos para el etiquetado
+   * @returns etiquetadoBeneficiarios
+   */
   const getEtiquedadoBeneficiarios = async () => {
     try {
-      /**
-       * ToDo: Se debe de cambiar el endpoint por el correcto
-       */
       const url = `${baseUrlPublico}emisiontarjetas/etiquetaTarjeta`;
       return new Promise((resolve, reject) => {
         axios
@@ -219,7 +221,7 @@ export const SolicitudEmbozoTarjetasContextProvider = (props) => {
       const url = `${baseUrlPublico}emisiontarjetas/registroEtiqueta`;
       return new Promise((resolve, reject) => {
         axios
-          .post(url,etiquetados, {
+          .post(url, etiquetados, {
             headers: {
               Accept: "application/json",
               "Content-type": "application/json",
@@ -274,6 +276,68 @@ export const SolicitudEmbozoTarjetasContextProvider = (props) => {
     }
   };
 
+  /**
+   * Busca a los beneficiarios por lote
+   * @returns etiquetadoBeneficiarios
+   */
+  const getEtiquedadoBeneficiariosLote = async (idLote) => {
+    try {
+      const url = `${baseUrlPublico}emisiontarjetas/etiquetaTarjetaLote/${idLote}`;
+      return new Promise((resolve, reject) => {
+        axios
+          .get(url, {
+            headers: {
+              Accept: "application/json",
+              "Content-type": "application/json",
+            },
+          })
+          .then((response) => {
+            console.log("ETIQUETADO BENEFICIARIOS: ", response.data);
+            resolve(response);
+            dispatch({
+              type: GET_ETIQUETADO_TARJETAS,
+              payload: response.data,
+            });
+          })
+          .catch((error) => {
+            /**
+             * se agrega esto para detectar el error, si no es necesario, borrar
+             */
+            console.log("Error ", error);
+            if (error.response) {
+              console.log("--------------------------------------------------");
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log("ERROR DATA", error.response.data);
+              console.log("ERROR STATUS", error.response.status);
+              console.log("ERROR HEADERS", error.response.headers);
+              dispatch({
+                type: GET_EMBOZO_TARJETAS,
+                payload: error.response,
+              });
+            } else if (error.request) {
+              console.log("*************************");
+
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log("ERROR REQUEST", error.request);
+            } else {
+              console.log("++++++++++++++++++++++++");
+              // Something happened in setting up the request that triggered an Error
+              console.log("Error MESSAGE ", error.message);
+            }
+            console.log(error.config);
+          });
+      });
+    } catch (error) {
+      dispatch({
+        type: AGREGAR_SOLICITUD_FOLIO_ERROR,
+        payload: false,
+      });
+    }
+  };
+
   return (
     <SolicitudEmbozoTarjetasContext.Provider
       value={{
@@ -281,8 +345,8 @@ export const SolicitudEmbozoTarjetasContextProvider = (props) => {
         etiquetadoBeneficiarios: state.etiquetadoBeneficiarios,
         getEmbozoBeneficiarios,
         guardarEmbozoTarjetas,
-        getEtiquedadoBeneficiarios,
-        guardarEtiquetadoTarjetas
+        guardarEtiquetadoTarjetas,
+        getEtiquedadoBeneficiariosLote
       }}
     >
       {props.children}
